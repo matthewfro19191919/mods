2342da6c3522b9b83557b590a27dee555c5d7252
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-16 08:58:21 -0500

    Update and rename godshaggy.lua to shaggy.lua

---
+++ scripts/shaggy.lua
@@ -0,0 +1,8 @@
+function onCreatePost()
+    if boyfriendName == 'shaggy' or boyfriendName == 'redshaggy' or boyfriendName  == 'supershaggy' or boyfriendName == 'godshaggy' then
+         setProperty("gfGroup.visible", false)
+   end
+    if boyfriendName == 'bf' or boyfriendName == 'dave-fnaf' then
+         setProperty("gfGroup.visible", true)
+    end
+end
