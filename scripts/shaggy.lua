f60a5a119c61d22251c869dbf510e1866c2aca69
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-03-23 01:53:53 -0400

    Update shaggy.lua

--- scripts/shaggy.lua
+++ scripts/shaggy.lua
@@ -1,6 +1,7 @@
 function onCreatePost()
     if boyfriendName == 'shaggy' or boyfriendName == 'redshaggy' or boyfriendName  == 'supershaggy' or boyfriendName == 'godshaggy' then
          setProperty("gfGroup.visible", false)
+         addLuaScript('extra keys hscript')
    end
     if boyfriendName == 'bf' or boyfriendName == 'dave-fnaf' then
          setProperty("gfGroup.visible", true)
